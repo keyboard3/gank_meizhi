9ed65a1e47035a809e710d95b57718b345833c45
keyboard3 <keyboard3@icloud.com>
2018-07-02 23:22:54 +0800

    Vue 版本初步创建

---
+++ vue/src/App.vue
@@ -0,0 +1,99 @@
+<template>
+	<div id="app">
+		<div class="header horizontal-layout">{{title}}</div>
+		<div class="horizontal-layout container">
+				<div class="vertical-layout box" v-for="item in list" :key="item._id">
+					<img :src="item.imageUrl"/>
+					<div class="title">{{item.desc}}</div>
+				</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'app',
+  data: function (){
+    return ({
+      title: '妹纸&gank.io',
+      list: []
+    });
+  },
+  created() {
+		this.loadData();
+  },
+  methods: {
+    loadData() {
+      let videosRequest = axios.get('http://gank.io/api/data/休息视频/10/1');
+      let welfaresRequest = axios.get('http://gank.io/api/data/福利/10/1');
+
+      axios.all([videosRequest, welfaresRequest])
+        .then(axios.spread((videosResult, welfaresResult) => {
+          let videos = videosResult.data.results;
+          let welfares = welfaresResult.data.results;
+          
+					videos.forEach((currentValue, index) => {
+						currentValue.imageUrl = welfares[index].url;
+						currentValue.desc = currentValue.publishedAt.substr(currentValue.publishedAt.indexOf("-") + 1, 5) 
+																+ currentValue.desc;
+					});
+					this.list = videos;
+        }));
+      },
+    }
+}
+</script>
+
+<style>
+[v-lock] {
+	display: none;
+}
+body{
+	margin: 0;
+	background: #F5FCFF;
+}
+.vertical-layout {
+	display: flex;
+	flex-direction: column;
+}
+.horizontal-layout {
+	display: flex;
+	flex-direction: row;
+}
+.header {
+	width: 100%;
+	height: 50px;
+	background: #191111;
+	color: white;
+	font-size: 14px;
+	position: fixed;
+}
+.header.horizontal-layout {
+	align-items: center;
+	padding-left: 8px;
+	margin-top: -50px;
+}
+.container {
+	flex-wrap: wrap;
+	justify-content: space-between;
+	padding: 0px 2.5vw;
+	margin-top: 50px;
+}
+img {
+	width: 45vw;
+}
+.title {
+	color: #626262;
+  font-size: 14px;
+  margin: 5px;
+}
+.box {
+	width: 45vw;
+  margin: 2px 0;
+  border-radius: 4px;
+  background-color: #fff;
+  border: 1px solid #CECECE;
+}
+</style>
